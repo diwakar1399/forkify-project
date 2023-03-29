ed1115ae4890b2aa6f6ca0ff6adab936f5aa1241
diwakar1399 <diwakarbharati6@gmail.com>
2023-03-29 10:29:48 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7bb50a3f-0ab8-4e22-879e-7ece756b5338';
+export const MODAL_CLOSE_SEC = 2.5;
